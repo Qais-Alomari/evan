5a992d38d5d78f3f14521a8ce5b50a31c1406d57
Qais Al Omari <omari.qais@gmail.com>
2018-06-02 00:18:29 +0300

    add new file

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyFirstJavaProgram {
+    public static void main(String []args) {
+        int x,y;
+        Scanner input= new Scanner(System.in);
+        x=input.next();
+        y=input.next();
+        int sum= x+y;
+    }
+}
