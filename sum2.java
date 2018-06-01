5f11a0e03529860c6420193126adcb20809de4b2
Qais Al Omari <omari.qais@gmail.com>
2018-06-02 01:19:59 +0300

    second file

--- sum2.java
+++ sum2.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class sum {
+public class sum2 {
     public static void main(String []args) {
         int x,y;
         Scanner input= new Scanner(System.in);
         x=input.nextInt();
         y=input.nextInt();
         int sum= x+y;
-        System.out.println(sum*2);
+        System.out.println(sum);
     }
 }
